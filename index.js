307db2e416a28f12136850d441ff30fabd6a7ac7
ander <ander.zubizarreta@ikasle.aeg.eus>
2024-09-24 12:28:18 +0200

    Connection with MongoDB database

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./src/routes/authRoutes');
 
+const mongoose = require('mongoose');
+const mongodbRoute = process.env.MONGO_URI;
+//import bodyParser from "body-parser";
+
+
 dotenv.config();
 
 const app = express();
+
+//Use bodyparser
+//app.use(bodyParser.json());
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -14,6 +22,30 @@ app.use('/api/token', authRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
+const PORT_MONGO = 27017;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+async function start() {
+  try {
+
+    await mongoose.connect(mongodbRoute);
+    app.listen(PORT_MONGO, () => {
+     
+      console.log(`Api is listenning on port ${PORT_MONGO}`);
+    })
+    console.log('Conexion con Mongo correcta');
+    
+    
+  } catch (error) {
+    console.log(`<<ERROR>> connecting to the database: ${error.message}`);
+    
+  }
+}
+
+start();
+
+
+
