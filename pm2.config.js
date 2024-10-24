9caa3f4d26ce0a44d99303f1461270e8137f31b8
izeta8 <julen.izeta@ikasle.aeg.eus>
2024-10-24 13:26:04 +0200

    [MENDI-87][UPDATE] PM2 CI Test

--- pm2.config.js
+++ pm2.config.js
@@ -4,8 +4,6 @@ module.exports = {
         name: "MendiHouse-Server",
         script: "./index.js",
         exec_mode: "cluster_mode",
-        // instances: 2,
-        // max_memory_restart: "300M",
   
         // Logging
         out_file: "./pm2/out.log",
