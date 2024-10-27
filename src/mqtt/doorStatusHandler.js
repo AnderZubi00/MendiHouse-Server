6fb57f435a43a71f294ac156f3e1d2b24e36dc33
aitor <aitor.mendiburu@ikasle.aeg.eus>
2024-10-27 13:20:03 +0100

    [MENDI-272][ADD]patch new isInsideTower of the acolyte in the DB

--- src/mqtt/doorStatusHandler.js
+++ src/mqtt/doorStatusHandler.js
@@ -1,4 +1,4 @@
-
+const { toggleIsInsideTowerByEmail } = require('../database/Player');
 
 function handleDoorAccess(mqttClient) {
 
@@ -19,11 +19,16 @@ function handleDoorAccess(mqttClient) {
           console.log(doorMessage);
           const doorStatus = doorMessage.msg;
           console.log(doorStatus);
+          const doorEmail = doorMessage.email;
+          console.log(doorEmail); //this is sent when published action doorOpen + email
+
+          //desde esp32 recibes message of opened door y para quien (asi evitamos que pase otro y le pasamos el email a BD para decir que eeee se abre solo para este email)
 
             if (doorStatus === 'opened') {
                 ///Consult in DB acolyte isInsideTower status (exiting or entering?)
-                ///socket to acolyte isInsideTower true/false to change screen
-                ///Patch in DB isInsideTower to true/false
+                //Patch to new value insInsideTower to opposite
+                //Socket new value to acolyte to change screen
+                toggleIsInsideTowerByEmail(doorEmail);
                 //Publish topic action with message close door
                 console.log('lets go');
             }
